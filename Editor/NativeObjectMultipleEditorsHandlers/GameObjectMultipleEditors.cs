90d93df16d115448d445ab22bf7a233b2bc737aa
Hubert GROSSIN <hubert.grossin@gmail.com>
2020-11-23 03:06:41 +0100

    Update changelog & package version

--- Editor/NativeObjectMultipleEditorsHandlers/GameObjectMultipleEditors.cs
+++ Editor/NativeObjectMultipleEditorsHandlers/GameObjectMultipleEditors.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
-using MuffinDev.EditorUtils;
+using MuffinDev.MultipleEditors.Utilities;
 
 using Object = UnityEngine.Object;
 
